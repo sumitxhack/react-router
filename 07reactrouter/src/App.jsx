636d25c70639d87dcaef7ef42aed412b9579a50d
sumitxhack <sumitssc93@gmail.com>
2026-08-17 20:20:06 +0530

    react router topic done

---
+++ 07reactrouter/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
+
+const App = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
